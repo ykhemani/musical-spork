540fde4aa58f11cd70afb8ce10f9e5ad58e98c5c
Austin Workman <austin@hashicorp.com>
2019-03-29 01:46:18 -0500

    Fixing startup issues and debug boolean

--- terraform/admin/nomad/application-deployment/spork-backendsvc/0_enable_debug.sh
+++ terraform/admin/nomad/application-deployment/spork-backendsvc/0_enable_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o xtrace
-consul kv put -datacenter=us-east-1 service/web/debug False
-consul kv put -datacenter=us-west-2 service/web/debug False
+consul kv put -datacenter=us-east-1 service/web/debug True
+consul kv put -datacenter=us-west-2 service/web/debug True
