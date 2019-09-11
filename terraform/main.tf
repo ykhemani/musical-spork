e58c4bd5ca48554ea3e83608adbe7ea0f78b6179
Yash Khemani <khemani@shabd-2.local>
2019-09-11 12:50:46 +0000

    specify version 1.x of AWS VPC module for use with terraform 0.11

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,7 @@ module "vpc-east" {
   }
 
   source                           = "terraform-aws-modules/vpc/aws"
+  version                          = "1.71.0"
   name                             = "${random_id.environment_name.hex}-east"
   cidr                             = "10.0.0.0/16"
   azs                              = ["us-east-1a", "us-east-1b", "us-east-1c"]
@@ -64,6 +65,7 @@ module "vpc-west" {
   }
 
   source                           = "terraform-aws-modules/vpc/aws"
+  version                          = "1.71.0"
   name                             = "${random_id.environment_name.hex}-west"
   cidr                             = "172.16.0.0/16"
   azs                              = ["us-west-2a", "us-west-2b", "us-west-2c"]
